9fa701e9f9c8c4e292c94b3af50fe224abf4d19e
Mary Ojo <38431519+maryojo@users.noreply.github.com>
2024-06-11 22:10:29 +0100

    Update to work with contentful api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { createClient } from 'contentful';
 import ResourceCard from './components/ResourceCard';
-import resources from './components/resources';
 
 const ITEMS_PER_PAGE = 15;
 
@@ -14,38 +13,43 @@ function App() {
   const [resources, setResources] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState('All');
   const [visibleItems, setVisibleItems] = useState(ITEMS_PER_PAGE);
+  const [loading, setLoading] = useState(null);
 
   useEffect(() => {
     const fetchResources = async () => {
+      setLoading(true);
       try {
-        const entry = await client.getEntry(process.env.REACT_APP_ENTRY_ID);
-        console.log(entry);
-        // setResources(items);
+        const entries = await client.getEntries();
+        setLoading(false);
+        setResources(entries.items);
       } catch (error) {
+        setLoading(false);
+        alert('Something went wrong. Please try again');
         console.error('Error fetching resources:', error);
       }
     };
     fetchResources();
   }, []);
 
-  const categories = ['All', ...new Set(resources.map(resource => resource.category))];
+  const categories = ['All', ...new Set(resources.flatMap(resource => resource.fields.category))];
 
   const filteredResources = selectedCategory === 'All' 
-    ? resources 
-    : resources.filter(resource => resource.category === selectedCategory);
+  ? resources 
+  : resources.filter(resource => resource.fields.category?.includes(selectedCategory));
 
   const handleLoadMore = () => {
     setVisibleItems(prevVisibleItems => prevVisibleItems + ITEMS_PER_PAGE);
   };
 
   return (
-    <div className="min-h-screen p-4 bg-gray-100">
-      <h1 className="text-3xl font-bold text-center mb-4">Resource Library</h1>
-      <div className="flex justify-center mb-4">
+    <div className="min-h-screen p-4 bg-gray-100 relative pb-20">
+      <h1 className="text-3xl font-bold text-center mb-2">Design Resource Library</h1>
+      <p className='text-center mb-4'>My personal collection of high quality design resources</p>
+      <div className="flex justify-center mb-10">
         {categories.map(category => (
           <button 
             key={category} 
-            className={`px-4 py-2 m-2 rounded-full ${selectedCategory === category ? 'bg-blue-500 text-white' : 'bg-gray-200'}`} 
+            className={`px-4 py-1 mx-2 text-sm rounded-full ${selectedCategory === category ? 'bg-[#ADBE99] text-black' : 'bg-neutral-200'}`} 
             onClick={() => { 
               setSelectedCategory(category);
               setVisibleItems(ITEMS_PER_PAGE);  // Reset visible items on category change
@@ -55,21 +59,30 @@ function App() {
           </button>
         ))}
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {filteredResources.slice(0, visibleItems).map((resource, index) => (
-          <ResourceCard key={index} resource={resource} />
-        ))}
-      </div>
-      {visibleItems < filteredResources.length && (
+      {loading ? (
         <div className="flex justify-center mt-4">
-          <button 
-            onClick={handleLoadMore} 
-            className="px-4 py-2 bg-blue-500 text-white rounded-full"
-          >
-            Load More
-          </button>
+          <div className="loader">Loading...</div>
+        </div>
+      ) : (
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {filteredResources.slice(0, visibleItems).map((resource, index) => (
+              <ResourceCard key={index} resource={resource} />
+            ))}
+          </div>
+          {visibleItems < filteredResources.length && (
+            <div className="flex justify-center mt-4">
+              <button 
+                onClick={handleLoadMore} 
+                className="px-4 py-2 bg-[#ADBE99] text-black rounded-full"
+              >
+                Load More
+              </button>
+            </div>
+          )}
         </div>
       )}
+      <p className='absolute right-2 bottom-2'>Collated with ❤️ by <a href='https://bento.me/mary-uxe/' target='_blank' rel='noreferrer' className='underline'>Mary</a></p>
     </div>
   );
 }
--- src/components/ResourceCard.js
+++ src/components/ResourceCard.js
@@ -1,24 +1,36 @@
 import React from 'react';
 
 function ResourceCard({ resource }) {
+  resource = resource.fields;
+
   return (
     <div className="bg-white p-4 rounded-lg shadow-lg relative group">
-      <div className="overflow-hidden rounded-lg">
+      <div className="overflow-hidden rounded-lg relative">
         <img 
-          src={resource.image} 
+          src={resource.displayImage.fields.file.url} 
           alt={resource.name} 
           className="object-cover w-full h-48 group-hover:opacity-75"
         />
         <a 
           href={resource.link} 
           className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity"
+          target='_blank'
+          rel='noreferrer'
         >
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full">Download</button>
+          <button className="bg-[#ADBE99] text-black px-4 py-2 rounded-full">Download</button>
         </a>
       </div>
-      <h2 className="mt-2 text-xl font-bold">{resource.name}</h2>
-      <p className="text-gray-600">{resource.category}</p>
-      <p className="text-gray-600">by {resource.author}</p>
+      <div className='flex flex-col gap-2'>
+        <h2 className="mt-2 text-xl font-bold">{resource.name}</h2>
+        <p className="text-gray-600">
+          {resource.category?.map((cat, index) => (
+            <span key={index} className="inline-block bg-neutral-200 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-700 mr-2">
+              {cat}
+            </span>
+          ))}
+        </p>
+        <p className="text-gray-600">by <a href={resource.creatorLink} target='_blank' rel='noreferrer' className={resource.creatorLink ? 'underline' : ''}> {resource.creator} </a></p>
+      </div>
     </div>
   );
 }
